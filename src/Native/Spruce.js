34f2f7d42346f564e4846435fb35ba19d3b5f8f8
Jono Mingard <reason.koan@gmail.com>
2017-04-05 21:08:40 +1200

    Update native module to 0.18 style

--- src/Native/Spruce.js
+++ src/Native/Spruce.js
@@ -1,31 +1,13 @@
-// make is a function that takes an instance of the
-// elm runtime
-// returns an object where:
-//      keys are names to be accessed in pure Elm
-//      values are either functions or values
-function make(elm) {
-    // If Native isn't already bound on elm, bind it!
-    elm.Native = elm.Native || {}
-    // then the same for our module
-    elm.Native.Spruce = elm.Native.Spruce || {}
+var _binary_koan$elm_spruce$Native_Spruce = function() {
 
-    // `values` is where the object returned by make ends up internally
-    // return if it's already set, since you only want one definition of
-    // values for speed reasons
-    if (elm.Native.Spruce.values) return elm.Native.Spruce.values
+function listen(address, program) {
+    console.log(address)
 
-    // return the object of your module's stuff!
-    return elm.Native.Spruce.values = {
-        listen(address, server) {
-            console.log(address, server)
-
-            return server
-        }
-    }
+    return program
 }
 
-// setup code for Spruce
-// Elm.Native.Spruce should be an object with
-// a property `make` which is specified above
-Elm.Native.Spruce = {};
-Elm.Native.Spruce.make = make;
+return {
+	toHtml: F2(listen)
+}
+
+}()
