7898903f6a22dcad1b112d4d2c930e6c03891e8b
Jono Mingard <reason.koan@gmail.com>
2017-04-07 22:07:24 +1200

    Remove ports from the basic example

--- examples/Basic.elm
+++ examples/Basic.elm
@@ -1,4 +1,4 @@
-port module Basic exposing (..)
+module Basic exposing (..)
 
 import Spruce exposing (..)
 
@@ -6,7 +6,3 @@ import Spruce exposing (..)
 main : Server
 main =
     listen "localhost:4000" []
-
-port listen2 : String -> Cmd msg
-
-port onRequest : (String -> msg) -> Sub msg
